b051ea9fc2ea2200332dd790a74e2eb03c67f3d2
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-02-27 14:25:39 +0530

    handeling unauthorised requests

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const passport = require('passport');
 const authRoute = require('./routes/AuthRoute');
 const profileRoute = require('./routes/profilesRoute');
@@ -14,25 +13,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//setting up mongoDB connection
-// let dev_db_user = 'mongodb://localhost/users'
-// const mongoDB = process.env.MONGODB_URI || dev_db_user;
-
-// mongoose.connect(mongoDB, { useNewUrlParser: true })
-//     .then(() => console.log("Connection Succesful"))
-//     .catch((err) => console.log(err));
-
-// mongoose.Promise = global.Promise;
-
-// let db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
 //initialize passport middleware
 app.use(passport.initialize());
 
 //services
 app.use('/api',authRoute);
 app.use('/profiles', profileRoute);
+
+//catch unauthorised acces
+app.use((err, req, res, next) => {
+    if (err.name == 'UnauthorizedError'){
+        res.status(401);
+        res.send({
+            message: err.name
+        })
+    }
+})
 
 //starting server
 app.listen(process.env.PORT || 5000, () => console.log('Server Started at 5000'))
