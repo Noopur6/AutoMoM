c4c9508a8937fa35993cd7a2bcb6da49b1f72eac
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-03-05 15:33:55 +0530

    integrated socket.io

--- app.js
+++ app.js
@@ -6,29 +6,39 @@ const profileRoute = require('./routes/profilesRoute');
 const meetingRoute=require('./routes/MeetingRoute');
 const roomRoute=require('./routes/VirtualRoomRoute');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+const socketioJwt = require('socketio-jwt');
 
 require('./models/db');
 require('./config/passport')
 const app = express();
+
+//socket.io websocket integration with app
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 //use cors middleware
 app.use(cors());
 
 //bodyParser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 //initialize passport middleware
 app.use(passport.initialize());
 
 //services
-app.use('/api',authRoute);
+app.use('/api', authRoute);
 app.use('/profiles', profileRoute);
 app.use('/meeting',meetingRoute);
 app.use('/virtualRoom', roomRoute);
+app.use('/meeting', meetingRoute)
 
 //catch unauthorised acces
 app.use((err, req, res, next) => {
-    if (err.name == 'UnauthorizedError'){
+    if (err.name == 'UnauthorizedError') {
         res.status(401);
         res.send({
             message: err.name
@@ -36,5 +46,29 @@ app.use((err, req, res, next) => {
     }
 })
 
+//websocket events
+// io.on('connection', function (socket) {
+//     console.log('user connected');
+//     socket.on('chat message', function (msg) {
+//         io.emit('chat message', msg);
+//     });
+// });
+
+io.sockets
+    .on('connection', socketioJwt.authorize({
+        secret: 'MY_SECRET',
+        timeout: 15000 // 15 seconds to send the authentication message
+
+    })).on('authenticated', function (socket) {
+        //this socket is authenticated, we are good to handle more events from it.
+        console.log('hello! ');
+        socket.on('chat message', function (msg) {
+            console.log(msg);
+            io.emit('chat message', msg);
+        });
+    });
+
 //starting server
-app.listen(process.env.PORT || 5000, () => console.log('Server Started at 5000'))
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
