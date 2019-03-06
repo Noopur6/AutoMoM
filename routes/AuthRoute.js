c427f15395e9384fa67c8afe53f32efc63740136
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-03-06 13:03:40 +0530

    FIX AM-09 -> password length must be 8

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -15,8 +15,8 @@ router.post('/register', [
         check('firstName', "First Name can contain alphabates only").isAlpha(), 
         check('lastName', "Last Name is required").isAlpha(), 
         check('lastName', "Last Name can contain alphabates only").exists(), 
-        check('password', "Password is required"),
-        check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long").exists().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+        check('password', "Password is required").exists().not().isEmpty(),
+        check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and eight char long").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,8}$/, "i"),
         check('email', "Invalid Email").isEmail()
     ] 
     ,authController.register);
@@ -24,8 +24,8 @@ router.post('/register', [
 router.post('/login',[
         check('email', "Email is required").exists().not().isEmpty(),
         check('email', "Invalid Email").isEmail(),
-        check('password', "Password is required").exists(),
-        check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long").exists().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+        check('password', "Password is required").exists().not().isEmpty(),
+        check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and eight char long").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,8}$/, "i"),
     ], authController.login);
 
 module.exports = router;
