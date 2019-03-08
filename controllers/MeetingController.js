0474fad94502cd6deaad683d6568d378992dbd78
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-03-08 11:50:39 +0530

    Fix: email error

--- controllers/MeetingController.js
+++ controllers/MeetingController.js
@@ -35,7 +35,7 @@ module.exports.meetingRequest= (req,res)=> {
             });
             commonUtility.sendMail(meetRequest.participantEmail, meetRequest.organizerEmail,
                 'Automom: Meeting has been scheduled', "Hey,<br><br>Your meeting has been scheduled. Please join the meeting.<br>Meeting Title –  "+meetRequest.agenda+"<br>Invited By – "+meetRequest.organizerEmail+
-                +"<br>Date – "+meetRequest.meetingDate+"<br>Start Time – "+meetRequest.startTime+"<br> End Time – "+meetRequest.endTime+"<br>Location – "+meetRequest.location+"<br><br>Thanks,<br>Team AutoMoM.");
+                "<br>Date – "+meetRequest.meetingDate+"<br>Start Time – "+meetRequest.startTime+"<br> End Time – "+meetRequest.endTime+"<br>Location – "+meetRequest.location+"<br><br>Thanks,<br>Team AutoMoM.");
     
         }
     });
@@ -54,7 +54,7 @@ module.exports.meetingList= (req,res)=> {
             {organizerEmail: email},
             {participantEmail: {$elemMatch:{$eq: email}}}
         ]},{ "_id": 1, "organizerEmail": 1, "participantEmail":1, "meetingDate":1, "startTime":1,
-         "endTime":1, "location":1, "agenda":1, "status":1}, function(err, meetings) {
+         "endTime":1, "location":1, "agenda":1, "status":1, "conversation":1}, function(err, meetings) {
         if (err){
             console.log(err);
             res.send({
@@ -113,13 +113,13 @@ module.exports.updateMeeting = function(req,res) {
             if(operation === "cancel"){
                 commonUtility.sendMail(meeting.participantEmail, meeting.organizerEmail,
                     'Automom: Meeting has been cancelled', "Hey,<br><br>Your meeting has been cancelled.<br>Meeting Title –  "+meeting.agenda+"<br>Invited By – "+meeting.organizerEmail+
-                    +"<br>Date – "+meeting.meetingDate+"<br>Start Time – "+meeting.startTime+"<br> End Time – "+meeting.endTime+"<br>Location – "+meeting.location+"<br><br>Thanks,<br>Team AutoMoM.");
+                    "<br>Date – "+meeting.meetingDate+"<br>Start Time – "+meeting.startTime+"<br> End Time – "+meeting.endTime+"<br>Location – "+meeting.location+"<br><br>Thanks,<br>Team AutoMoM.");
                     
             }
             else {
                 commonUtility.sendMail(meeting.participantEmail, meeting.organizerEmail,
                     'Automom: Meeting has been re-scheduled', "Hey,<br><br>Your meeting has been re-scheduled. Please join the meeting.<br>Meeting Title –  "+meeting.agenda+"<br>Invited By – "+meeting.organizerEmail+
-                    +"<br>Date – "+meeting.meetingDate+"<br>Start Time – "+meeting.startTime+"<br> End Time – "+meeting.endTime+"<br>Location – "+meeting.location+"<br><br>Thanks,<br>Team AutoMoM.");
+                    "<br>Date – "+meeting.meetingDate+"<br>Start Time – "+meeting.startTime+"<br> End Time – "+meeting.endTime+"<br>Location – "+meeting.location+"<br><br>Thanks,<br>Team AutoMoM.");
                 
             }
         }        
