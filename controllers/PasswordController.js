aceb266c8b74749712bd533cb0c67d952064e6cb
Singh <Noopur.Singh@capco.com>
2019-03-20 15:26:29 +0530

    added change password and reset password api

--- controllers/PasswordController.js
+++ controllers/PasswordController.js
@@ -6,24 +6,105 @@ const emailTemplate = require('../email-templates/forget-pass');
 const crypto = require('crypto');
 const key = crypto.randomBytes(32);
 
-function encrypt(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), Buffer.from('3c1972b10646fdee4525b0bc5eebdadf', 'hex'));
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return encrypted.toString('hex');
-}
-
-function decrypt(text) {
-    let iv = Buffer.from('3c1972b10646fdee4525b0bc5eebdadf', 'hex');
-    let encryptedText = Buffer.from(text, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
-}
+// module.exports.reset = (req, res) => {
+//     res.sendFile('resetview.html', { root: path.join(__dirname, '../views') })
+// }
 
 module.exports.reset = (req, res) => {
-    res.sendFile('resetview.html', { root: path.join(__dirname, '../views') })
+    const errors=validationResult(req);
+    let flag=errors.isEmpty();
+    if(!flag){
+        return res.send({status:'E', message: 'Validations failed',error: errors.array({ onlyFirstError: true })});
+    }
+    let token = req.body.token;
+    User.findOne({'resetToken' : token}, function(err, user){
+        if(err) {
+            res.send({
+                status:"E",
+                error: [
+                    {
+                        msg: err
+                    }
+                ]
+            });
+        }else if(user == null) {
+            return res.send({
+                status:"E",
+                message:"Invalid token"
+            });
+        } else {
+            let diff = Math.round(Math.abs((new Date(decrypt(token)).getTime() - new Date().getTime())/(24*60*60*1000)));
+            if(diff<0 || diff>2){
+                return res.send({
+                    status:"E",
+                    message:"Token expired"
+                });
+            }
+            user.setPassword(req.body.password);
+            user.resetToken = null;
+            User.updateOne({resetToken : token}, user, function(err){
+                if(err) {
+                    res.send({
+                        status:"E",
+                        error: [
+                            {
+                                msg: err
+                            }
+                        ]
+                    });
+                } else {
+                    res.send({
+                        status:"C",
+                        message:"Success"
+                    });
+                }
+            });
+        }
+    });
+}
+
+module.exports.change = (req, res) => {
+    const errors=validationResult(req);
+    let flag=errors.isEmpty();
+    if(!flag){
+        return res.send({status:'E', message: 'Validations failed',error: errors.array({ onlyFirstError: true })});
+    }
+    User.findOne({email : req.body.email}, function(err, user){
+        if(err) {
+            res.send({
+                status:"E",
+                error: [
+                    {
+                        msg: err
+                    }
+                ]
+            });
+        }else if(user == null) {
+            return res.send({
+                status:"E",
+                message:"User not found"
+            });
+        } else {
+            user.setPassword(req.body.password);
+            User.updateOne({email : req.body.email}, user, function(err){
+                if(err) {
+                    res.send({
+                        status:"E",
+                        error: [
+                            {
+                                msg: err
+                            }
+                        ]
+                    });
+                } else {
+                    res.send({
+                        status:"C",
+                        message:"Success"
+                    });
+                }
+            });
+        }
+    });
 }
 
 module.exports.forgot = (req, res) => {
@@ -35,7 +116,8 @@ module.exports.forgot = (req, res) => {
 
     let encryptedToken = encrypt(new Date().toISOString());
     
-    User.findOneAndUpdate({email : req.body.email}, {$set:{resetToken:encryptedToken}}, { returnNewDocument: true }, function(err, user){
+    User.findOneAndUpdate({email : req.body.email}, {$set:{resetToken:encryptedToken}}, { returnNewDocument: true }, 
+        function(err, user){
         if(err) {
             res.send({
                 status:"E",
@@ -60,7 +142,24 @@ module.exports.forgot = (req, res) => {
             let url = 'https://automom-dev.herokuapp.com/password/reset/'+encryptedToken;
 
             //send mail
-            commonUtils.sendMail(req.body.email, null, 'AutoMoM: Reset Password', emailTemplate.bringThatForgotPasswordTemplate(url, req.body.email));
+            commonUtils.sendMail(req.body.email, null, 'AutoMoM: Reset Password', 
+            emailTemplate.bringThatForgotPasswordTemplate(url, req.body.email));
         }
     }); 
+}
+
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), Buffer.from('3c1972b10646fdee4525b0bc5eebdadf', 'hex'));
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString('hex');
+}
+
+function decrypt(text) {
+    let iv = Buffer.from('3c1972b10646fdee4525b0bc5eebdadf', 'hex');
+    let encryptedText = Buffer.from(text, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
 }
--- routes/PasswordRoute.js
+++ routes/PasswordRoute.js
@@ -11,11 +11,26 @@ const auth = jwt({
 })
 
 //reset password screen
-router.get('/reset/:uniqueId', passwordController.reset);
+//router.get('/reset/:uniqueId', passwordController.reset);
 
 router.post('/forgot',[
     check('email', "Email is required").exists().not().isEmpty(),
     check('email', "Invalid Email").isEmail()
 ], passwordController.forgot);
+
+router.post('/reset', [
+    check('password', "Password is required").exists().not().isEmpty(),
+    check("password", "Password should contain 1 special char,1 upper case letter, 1 number and length"+
+    "of password should be eight characters only").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,8}$/, "i").not().contains(' '),
+    check('token', "Token is required").exists().not().isEmpty()    
+], passwordController.reset);
+
+router.put('/new',[
+    check('email', "Email is required").exists().not().isEmpty(),
+    check('email', "Invalid Email").isEmail(),
+    check('password', "Password is required").exists().not().isEmpty(),
+    check("password", "Password should contain 1 special char,1 upper case letter, 1 number and length"+
+    "of password should be eight characters only").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,8}$/, "i").not().contains(' ')
+], auth, passwordController.change);
 
 module.exports = router;
